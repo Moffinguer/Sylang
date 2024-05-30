7c964419af18e2293eeb8272501675d8cc2762ac
Moffinguer <fernandomateos.fermatgom@gmail.com>
2024-05-30 18:04:41 +0200

    Create functions.c

---
+++ tests/expected/functions.c
@@ -0,0 +1,16 @@
+void pintaTexto()
+{
+    printf("Hola Mundo");
+}
+
+double multiplicaNumeros(int a, int b)
+{
+    double multiplicacion = a * b;
+    return multiplicacion;
+}
+
+int main()
+{
+    pintaTexto();
+    double multiplicacion = multiplicaNumeros(2,5);
+}
